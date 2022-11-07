e7967805874bd235ff03a5d0d88c8797ec217e4e
hoon2 <sanghoon.engineer@gmail.com>
2022-11-07 21:40:14 +0900

    hero 개발

--- src/theme.js
+++ src/theme.js
@@ -6,7 +6,7 @@ const theme = extendTheme({
   styles: {
     global: {
       body: {
-        backgroundColor: "black",
+        backgroundColor: "blackAlpha.900",
         color: "white",
       },
     },
